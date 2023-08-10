d21f4c002c970390e54ad637ae857a9f7e1a7039
PamelaUjah <pamelat008@hotmail.com>
2023-08-10 18:07:37 +0100

    Refactoring and added Customer entity and test class.

---
+++ src/main/java/com/clearscore/creditcards/Customer.java
@@ -0,0 +1,28 @@
+package com.clearscore.creditcards;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //generates getters and setters
+@Builder //creates instance of Customer
+@AllArgsConstructor
+public class Customer {
+    //TODO add regex for full name
+    @NotBlank(message = "Full Name is mandatory")
+    @Schema(name = "Full Name", example = "John Smith", description = "Users full name")
+    private String name;
+
+    @Min(0)
+    @Max(700)
+    @Schema(name = "Credit Score", example = "John 341", description = "Credit score between 0 and 700")
+    private Integer creditScore;
+
+    @Min(0)
+    @Schema(name = "Salary", example = "28000", description = "Users annual salary")
+    private Integer salary;
+
+}
---
+++ src/test/java/com/clearscore/testcreditcards/TestCustomer.java
@@ -0,0 +1,76 @@
+package com.clearscore.testcreditcards;
+
+import com.clearscore.creditcards.Customer;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class TestCustomer {
+
+    public static final String VALID_FULL_NAME = "Jasmine Lincoln";
+    public static final int VALID_CREDIT_SCORE = 200;
+    public static final int VALID_SALARY = 31000;
+    public static final String INVALID_FULL_NAME = " ";
+    public static final int INVALID_CREDIT_SCORE = 750;
+    public static final int INVALID_SALARY = -1;
+
+    private Customer customer;
+    private static Validator validator;
+
+    @BeforeAll
+    public static void setupValidatorInstance() {
+        validator = Validation.buildDefaultValidatorFactory()
+                .getValidator();
+    }
+    @Test
+    @DisplayName("Given valid customer details, when customer details are set, then there are no violations ")
+    void testCustomerDetailsAreValid() {
+        givenCustomerDetails();
+
+        whenCustomerDetailsAreSet(VALID_FULL_NAME, VALID_CREDIT_SCORE, VALID_SALARY);
+
+        thenNoErrorIsLogged();
+    }
+
+    @Test
+    @DisplayName("Given invalid customer details, when customer details are set, then there are violations")
+    void testInvalidCustomerDetails() {
+        givenCustomerDetails();
+
+        whenCustomerDetailsAreSet(INVALID_FULL_NAME, INVALID_CREDIT_SCORE, INVALID_SALARY);
+        
+        Set<ConstraintViolation<Customer>> violations = validator.validate(customer);
+        assertThat(violations.size()).isEqualTo(3);
+    }
+
+    void givenCustomerDetails() {
+
+    }
+
+    void whenCustomerDetailsAreSet(String fullName, Integer creditScore, Integer salary) {
+        customer = new Customer(fullName, creditScore, salary);
+    }
+
+    private void thenNoErrorIsLogged() {
+        assertThat(customer
+                .getName()).isEqualTo(VALID_FULL_NAME)
+                .isNotBlank()
+                .isNotNull();
+        assertThat(customer.getSalary())
+                .isEqualTo(VALID_SALARY)
+                .isNotNull();
+
+        assertThat(customer.getCreditScore())
+                .isEqualTo(VALID_CREDIT_SCORE)
+                .isNotNull();
+    }
+
+}
